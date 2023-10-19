d2a50460359553a7cac74e98da18cc9022b6346d
luisfe2127 <luis.canarinho2127@gmail.com>
2023-10-19 14:14:30 -0300

    atualizando cardapio

---
+++ Texto/script.js
@@ -0,0 +1,9 @@
+const c1 = document.getElementById('card01')
+const c2 = document.getElementById('card02')
+const c3 = document.getElementById('card03')
+const c4 = document.getElementById('card04')
+
+function card01() {
+    c1.innerHTML = 'Hello'
+
+}
