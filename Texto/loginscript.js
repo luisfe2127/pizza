9f4a5f824058133f762345963fcdbd60b11982ac
luisfe2127 <luis.canarinho2127@gmail.com>
2023-11-17 12:59:08 -0300

    atualizando login

--- Texto/loginscript.js
+++ Texto/loginscript.js
@@ -2,4 +2,13 @@ function enviar() {
     document.getElementById('ienviar')
     document.getElementById('iemail')
     document.getElementById('isenha')
+
+    let txtsenha = document.getElementById('iemail')
+    let txtemail = document.getElementById('isenha')
+
+    if (txtsenha, txtemail > 8) {
+        alert('Certo')
+    } else {
+        alert('Errado')
+    }
 }
